0e54b8633fefa7a7a16c194730b3bc1dc02927ad
zekpause <samson6603@gmail.com>
2022-03-01 19:17:14 +0800

    Picture in Picture

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+
+async function selectMediaStream() {
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata= () => {
+            videoElement.onplay();
+
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+
+});
+
+// OnLoad
+selectMediaStream();
